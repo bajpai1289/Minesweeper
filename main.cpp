0b8f542f01e96a6f67d6d671be34f47d9c73a32e
Abhishek Bajpai <58601525+bajpai1289@users.noreply.github.com>
2020-04-03 17:52:11 +0530

    added comments
    added comments for easier understanding

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@ void initbombs(int bombs);  /* initializes the random position for the bombs */
 int disclosecell(int row, int col);  /* opens the cells */
 void thegameover();  /* displays the final state of the game */
 
-/* setbkcolour(): function of graphics.h, responsible for changing background color */
 void background(int colour){
     setfillstyle(SOLID_FILL, colour);
     bar(0, 0, W-1, H-1);
@@ -22,7 +21,6 @@ void background(int colour){
 }
 
 int main(){
-    /* number of bombs: 14.5% of total cells are to be bomb */
     int numofbombs = ROWS*COLS*29.0/200.0;
 
     initwindow(W, H, "HELLO");
@@ -35,8 +33,8 @@ int main(){
 
 void* (sprites[SPRITES_NUM]);
 const int cellw = W/COLS, cellh = H/ROWS;  /* choose the WIDTH & HEIGHT and ROWS & COLS such that cellw and cellh will be equal */
-int cellnum[ROWS+2][COLS+2];  /* to keep all the details of cell */
-char cellstate[ROWS+2][COLS+2];  /* to keep track of whether the cell is opened or closed */
+int cellnum[ROWS+2][COLS+2];  
+char cellstate[ROWS+2][COLS+2]; 
 enum{
     BLANK, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, BOMB, FLAG, WFLAG, CLOSED, OPENED
 };
@@ -62,20 +60,17 @@ void theGame(int numofbombs){
             my = mr*cellh;
         }
 
-        /* click left mouse button to open the cells */
         if(ismouseclick(WM_LBUTTONDOWN) && cellstate[mr+1][mc+1] == CLOSED){
             cellstate[mr+1][mc+1] = OPENED;
             exposedcells++;
             putimage(mx, my, sprites[cellnum[mr+1][mc+1]], COPY_PUT);
             if(cellnum[mr+1][mc+1] == BOMB){
-                /* gameover(LOSS): if the clicked cell has bomb */
                 thegameover();
                 gameover = true;
             }
             else if(cellnum[mr+1][mc+1] == BLANK)
                 exposedcells += disclosecell(mr+1, mc+1);
         }
-        /* click right mouse button to flag the cells */
         else if(ismouseclick(WM_RBUTTONDOWN) && cellstate[mr+1][mc+1] != OPENED){
             if(cellstate[mr+1][mc+1] == FLAG){
                 cellstate[mr+1][mc+1] = CLOSED;
@@ -92,7 +87,6 @@ void theGame(int numofbombs){
         clearmouseclick(WM_RBUTTONDOWN);
         delay(48);
 
-        /* gameover(WIN): if the number of opened cells equals total number of cells without bombs */
         if(exposedcells == (ROWS*COLS - numofbombs))
             gameover = true;
 
@@ -111,7 +105,6 @@ void theGame(int numofbombs){
 void pre_loading(void* sprites[]){
     unsigned int img_size;
 
-    /* names of the sprites */
     char sprite_names[SPRITES_NUM][24] = {"sprites/blank.ico", "sprites/1.ico",
                                           "sprites/2.ico", "sprites/3.ico",
                                           "sprites/4.ico", "sprites/5.ico", "sprites/6.ico",
@@ -122,7 +115,6 @@ void pre_loading(void* sprites[]){
 
     setactivepage(ONE);
 
-    /* loading the sprites from disk to RAM */
     for(int i = 0; i<SPRITES_NUM; i++){
     	
         readimagefile(sprite_names[i], 0, 0, cellw-1, cellh-1);
@@ -131,7 +123,6 @@ void pre_loading(void* sprites[]){
     }
     cleardevice();
 
-    /* displaying the closed cells */
     for(int px = 0; px < W; px+=cellw){
         for(int py = 0; py < H; py+=cellh){
             putimage(px, py, sprites[CLOSED], COPY_PUT);
@@ -146,20 +137,16 @@ void pre_loading(void* sprites[]){
 
 void initgrid(int numofbombs){
 
-    /* seeding the random function */
     srand(time(NULL));
 
     for(int i = 0; i < ROWS + 2; i++){
         for(int j = 0; j < COLS + 2; j++){
-            /* Setting the grids */
             cellnum[i][j] = BLANK;
             cellstate[i][j] = CLOSED;
         }
-        /* Setting the LEFT and the RIGHT boundary of both grids accordingly*/
         cellstate[i][COLS + 1] = cellstate[i][0] = OPENED;
     }
 
-    /* Setting the TOP and the BOTTOM boundary of both grids accordingly*/
     for(int j = 0; j < COLS + 2; j++){
         cellstate[ROWS + 1][j] = cellstate[0][j] = OPENED;
     }
@@ -172,25 +159,23 @@ void initgrid(int numofbombs){
 
 void initbombs(int bombs){
 
-    if(!bombs)  /* terminating condition: if amounts of bombs is zero */
+    if(!bombs) 
         return;
     else{
         int bombposx, bombposy;
 
-        /* loop continues until random position selected doesn't have bomb */
-        while(cellnum[bombposy = (1 + rand()%ROWS)][bombposx = (1 + rand()%COLS)] == BOMB)
-            ;
+        while(cellnum[bombposy = (1 + rand()%ROWS)][bombposx = (1 + rand()%COLS)] == BOMB);
 
-        /* assigning bomb to the selected random position */
+ 
         cellnum[bombposy][bombposx] = BOMB;
 
-        /* applying recursion to mark other bombs before incrementing the neighbors */
+
         initbombs(bombs - 1);
 
         for(int j = bombposy - 1; j < bombposy + 2; j++)
             for(int k = bombposx - 1; k < bombposx + 2; k++)
                 if(cellnum[j][k] != BOMB)
-                    cellnum[j][k]++;  /* increments the neighbors of the bomb only if they aren't bombs themselves */
+                    cellnum[j][k]++;  
 
     }
     return;
